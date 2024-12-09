4f3f2046d4ca94d2714812469f0a02fbfb036720
David Paparo <d.paparo@tiscali.it>
2024-12-09 17:48:52 +0100

    asercizio completo

--- main.js
+++ main.js
@@ -1,8 +1,54 @@
 
 
-let saluto = `ciao`
+// Metodo  if/else
+let voto = 29;
 
-saluto = `ciaone`
+if(voto <18 && voto >=0){
+    console.log(`Insufficiente`);
+} else if ( voto >=18 && voto <21) {
+    console.log(`Sufficiente`);
+} else if ( voto >=21 && voto <24) {
+    console.log(`Buono`);
+} else if ( voto >=24 && voto <27 ) {
+    console.log(`Distinto`);
+} else if ( voto >=27 && voto <=29 ) {
+    console.log(`Ottimo`);
+} else if ( voto == 30 ) {
+    console.log(`Eccellente`);
+} else {
+    console.log(`valore fuori "range"`);
+    
+}
 
-console.log(saluto);
+
+
+
+// Metodo switch
+let voto_x = 30;
+
+switch (true){
+    case voto_x <18 && voto_x >=0:
+        console.log(`Insufficiente`);
+        break;
+
+    case voto_x >=18 && voto_x <21:
+        console.log(`Sufficiente`);
+        break;
+
+    case voto_x >=21 && voto_x <24:
+        console.log(`Buono`);
+        break;
+
+    case voto_x >=24 && voto_x <27:
+        console.log(`Ottimo`);
+        break;
+
+    case voto_x == 30 :
+        console.log(`Eccellente`);
+        break;
+
+    default:
+        console.log(`valore fuori "range"`);
+}
+
 
